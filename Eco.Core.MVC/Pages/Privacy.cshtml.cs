18136e71711aa6c048d23df8df9dc927bb71000d
jiangyuanqiao <yuanqjiang@deloitte.com.cn>
2023-03-27 16:24:05 +0800

    添加项目文件。

---
+++ Eco.Core.MVC/Pages/Privacy.cshtml.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Eco.Core.MVC.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+        private readonly ILogger<PrivacyModel> _logger;
+
+        public PrivacyModel(ILogger<PrivacyModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+        }
+    }
+}
