e635eca1a0c2ca9ae599eef5811ef995fec3c982
jiangyuanqiao <yuanqjiang@deloitte.com.cn>
2023-03-28 13:52:10 +0800

    1

--- Eco.Core.MVC/Program.cs
+++ Eco.Core.MVC/Program.cs
@@ -8,7 +8,7 @@ namespace Eco.Core.MVC
 
             // Add services to the container.
             builder.Services.AddRazorPages();
-
+             
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
